5971b9b5f427eb1b9ff67df3e65222f68ce82983
S. Davilla <davilla@4pi.com>
2011-04-05 02:10:19 -0400

    [ce41xx] refactor depends... again

--- tools/ce41xx/setup-sdk.sh
+++ tools/ce41xx/setup-sdk.sh
@@ -3,27 +3,27 @@
 SCRIPT_PATH=$(cd `dirname $0` && pwd)
 
 BUILDROOT=${INTELCE_BUILDROOT:-$HOME/IntelCE-20.0.11052.243197}
-XBMCPREFIX=${INTELCE_XBMCPREFIX:-/opt/xbmc-ce41xx}
+XBMCSTAGE=${INTELCE_XBMCPREFIX:-/opt/xbmc-ce41xx}
 #
-sudo mkdir -p $XBMCPREFIX
-sudo chmod 777 $XBMCPREFIX
-mkdir -p $XBMCPREFIX/local/lib
-mkdir -p $XBMCPREFIX/local/include
+sudo mkdir -p $XBMCSTAGE
+sudo chmod 777 $XBMCSTAGE 
+mkdir -p $XBMCSTAGE/usr/local/lib
+mkdir -p $XBMCSTAGE/usr/local/include
 #
-rm -f $XBMCPREFIX/IntelCE
-rm -f $XBMCPREFIX/toolchains
-rm -f $XBMCPREFIX/targetfs
-ln -s $BUILDROOT/build_i686/staging_dir $XBMCPREFIX/IntelCE
-ln -s $BUILDROOT/build_i686/staging_dir/bin $XBMCPREFIX/toolchains
-ln -s $BUILDROOT/project_build_i686/IntelCE/root $XBMCPREFIX/targetfs
+rm -f $XBMCSTAGE/IntelCE
+rm -f $XBMCSTAGE/toolchains
+rm -f $XBMCSTAGE/targetfs
+ln -s $BUILDROOT/build_i686/staging_dir $XBMCSTAGE/IntelCE
+ln -s $BUILDROOT/build_i686/staging_dir/bin $XBMCSTAGE/toolchains
+ln -s $BUILDROOT/project_build_i686/IntelCE/root $XBMCSTAGE/targetfs
 #  hide config.h in Intel's SDK
-if [ -f "$XBMCPREFIX/IntelCE/include/config.h" ] ; then
-  mv $XBMCPREFIX/IntelCE/include/config.h $XBMCPREFIX/IntelCE/include/config.h.orig
+if [ -f "$XBMCSTAGE/IntelCE/include/config.h" ] ; then
+  mv $XBMCSTAGE/IntelCE/include/config.h $XBMCSTAGE/IntelCE/include/config.h.orig
 fi
 #
-echo "XBMCPREFIX=$XBMCPREFIX"                                          >  $SCRIPT_PATH/Makefile.include
+echo "XBMCSTAGE=$XBMCSTAGE"                                            >  $SCRIPT_PATH/Makefile.include
 echo "BASE_URL=http://mirrors.xbmc.org/build-deps/darwin-libs"         >> $SCRIPT_PATH/Makefile.include
-echo "TARBALLS_LOCATION=$XBMCPREFIX/tarballs"                          >> $SCRIPT_PATH/Makefile.include
+echo "TARBALLS_LOCATION=$XBMCSTAGE/tarballs"                           >> $SCRIPT_PATH/Makefile.include
 echo "RETRIEVE_TOOL=/usr/bin/curl"                                     >> $SCRIPT_PATH/Makefile.include
 echo "RETRIEVE_TOOL_FLAGS=-Ls --create-dirs --output \$(TARBALLS_LOCATION)/\$(ARCHIVE)" >> $SCRIPT_PATH/Makefile.include
 echo "ARCHIVE_TOOL=/bin/tar"                                           >> $SCRIPT_PATH/Makefile.include
