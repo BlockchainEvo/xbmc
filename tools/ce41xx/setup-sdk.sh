39488da75f7873e57a1a7f66d5f2c134a5b1ebf5
theuni <theuni-nospam-@xbmc.org>
2011-05-26 01:26:51 -0400

    [ce41xx] Changed the build system around some.
    Now we build in /opt/xbmc-cex by default. This way we can share paths
    between our build system and target system without worrying about rpath
    and friends. Also, it is now very easy to mount /opt/xbmc-cex via a
    separate nfs share, so that no files have to be copied in order to test
    a build.

--- tools/ce41xx/setup-sdk.sh
+++ tools/ce41xx/setup-sdk.sh
@@ -2,28 +2,31 @@
 
 SCRIPT_PATH=$(cd `dirname $0` && pwd)
 
-BUILDROOT=${INTELCE_BUILDROOT:-$HOME/IntelCE-20.0.11052.243197}
-XBMCSTAGE=${INTELCE_XBMCPREFIX:-/opt/xbmc-ce41xx}
+#Edit these two
+BUILDROOT=${INTELCE_BUILDROOT:-$HOME/IntelCE-21.0.11124.264601}
+TARBALLS=/opt/xbmc-tarballs
+
+XBMCPREFIX=${INTELCE_XBMCPREFIX:-/opt/xbmc-cex}
+SDKSTAGE=$BUILDROOT/build_i686/staging_dir
+TARGETFS=$BUILDROOT/project_build_i686/IntelCE/root
+TOOLCHAIN=$BUILDROOT/build_i686/staging_dir/bin
 #
-sudo mkdir -p $XBMCSTAGE
-sudo chmod 777 $XBMCSTAGE 
-mkdir -p $XBMCSTAGE/usr/local/lib
-mkdir -p $XBMCSTAGE/usr/local/include
+sudo mkdir -p $XBMCPREFIX
+sudo chmod 777 $XBMCPREFIX
+mkdir -p $XBMCPREFIX/lib
+mkdir -p $XBMCPREFIX/include
 #
-rm -f $XBMCSTAGE/IntelCE
-rm -f $XBMCSTAGE/toolchains
-rm -f $XBMCSTAGE/targetfs
-ln -s $BUILDROOT/build_i686/staging_dir $XBMCSTAGE/IntelCE
-ln -s $BUILDROOT/build_i686/staging_dir/bin $XBMCSTAGE/toolchains
-ln -s $BUILDROOT/project_build_i686/IntelCE/root $XBMCSTAGE/targetfs
 #  hide config.h in Intel's SDK
-if [ -f "$XBMCSTAGE/IntelCE/include/config.h" ] ; then
-  mv $XBMCSTAGE/IntelCE/include/config.h $XBMCSTAGE/IntelCE/include/config.h.orig
+if [ -f "$SDKSTAGE/IntelCE/include/config.h" ] ; then
+  mv $SDKSTAGE/include/config.h $SDKSTAGE/include/config.h.orig
 fi
 #
-echo "XBMCSTAGE=$XBMCSTAGE"                                            >  $SCRIPT_PATH/Makefile.include
+echo "SDKSTAGE=$SDKSTAGE"                                              >  $SCRIPT_PATH/Makefile.include
+echo "XBMCPREFIX=$XBMCPREFIX"                                          >> $SCRIPT_PATH/Makefile.include
+echo "TARGETFS=$TARGETFS"                                              >> $SCRIPT_PATH/Makefile.include
+echo "TOOLCHAIN=$TOOLCHAIN"                                            >> $SCRIPT_PATH/Makefile.include
 echo "BASE_URL=http://mirrors.xbmc.org/build-deps/darwin-libs"         >> $SCRIPT_PATH/Makefile.include
-echo "TARBALLS_LOCATION=$XBMCSTAGE/tarballs"                           >> $SCRIPT_PATH/Makefile.include
+echo "TARBALLS_LOCATION=$TARBALLS"                                     >> $SCRIPT_PATH/Makefile.include
 echo "RETRIEVE_TOOL=/usr/bin/curl"                                     >> $SCRIPT_PATH/Makefile.include
 echo "RETRIEVE_TOOL_FLAGS=-Ls --create-dirs --output \$(TARBALLS_LOCATION)/\$(ARCHIVE)" >> $SCRIPT_PATH/Makefile.include
 echo "ARCHIVE_TOOL=/bin/tar"                                           >> $SCRIPT_PATH/Makefile.include
