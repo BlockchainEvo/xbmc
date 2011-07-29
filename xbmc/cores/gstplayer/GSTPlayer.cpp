6d512e7796679c96bafb9f83747b76a994557383
S. Davilla <davilla@4pi.com>
2011-07-28 22:01:55 -0400

    [cex] remove stale code

--- xbmc/cores/gstplayer/GSTPlayer.cpp
+++ xbmc/cores/gstplayer/GSTPlayer.cpp
@@ -44,22 +44,6 @@
 #include <gst/tag/tag.h>
 #include <gst/pbutils/missing-plugins.h>
 
-// GstPlayFlags flags from playbin2. It is the policy of GStreamer to
-// not publicly expose element-specific enums. That's why this
-// GstPlayFlags enum has been copied here.
-typedef enum {
-  GST_PLAY_FLAG_VIDEO         = (1 << 0),
-  GST_PLAY_FLAG_AUDIO         = (1 << 1),
-  GST_PLAY_FLAG_TEXT          = (1 << 2),
-  GST_PLAY_FLAG_VIS           = (1 << 3),
-  GST_PLAY_FLAG_SOFT_VOLUME   = (1 << 4),
-  GST_PLAY_FLAG_NATIVE_AUDIO  = (1 << 5),
-  GST_PLAY_FLAG_NATIVE_VIDEO  = (1 << 6),
-  GST_PLAY_FLAG_DOWNLOAD      = (1 << 7),
-  GST_PLAY_FLAG_BUFFERING     = (1 << 8),
-  GST_PLAY_FLAG_DEINTERLACE   = (1 << 9)
-} GstPlayFlags;
-
 struct INT_GST_VARS
 {
   bool                    inited;
