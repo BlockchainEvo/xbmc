956e8b7e3b581f703bd53c4082eb137ddf807421
Cory Fields <theuni-nospam-@xbmc.org>
2013-03-12 04:11:25 -0400

    jni: register our receiver dynamically rather than in the manifest
    Some events must be done this way (battery level for example), so we may as
    well do them all this way.

--- tools/android/packaging/xbmc/src/org/xbmc/xbmc/Main.java
+++ tools/android/packaging/xbmc/src/org/xbmc/xbmc/Main.java
@@ -3,6 +3,9 @@ package org.xbmc.xbmc;
 import android.app.NativeActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import org.xbmc.xbmc.XBMCBroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
 
 public class Main extends NativeActivity 
 {
@@ -15,5 +18,10 @@ public class Main extends NativeActivity
   public void onCreate(Bundle savedInstanceState) 
   {
     super.onCreate(savedInstanceState);
+    IntentFilter intentFilter = new IntentFilter();
+    intentFilter.addDataScheme("file");
+    intentFilter.addAction(Intent.ACTION_MEDIA_MOUNTED);
+    XBMCBroadcastReceiver broadcastReceiver = new XBMCBroadcastReceiver();
+    registerReceiver(broadcastReceiver, intentFilter);
   }
 }
